f531f535b03e9dddc64bfceb3fe793a7c0f8eecf
POPO530 <horiuchi.8210@gmail.com>
2026-06-28 15:32:35 +0900

    不要ファイル削除と修正

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,7 +4,7 @@ import Header from './pages/Header';
 import Footer from './pages/Footer';
 import Game from './pages/Game';
 import Kuji from './pages/Kuji';
-
+import './index.css';
 import './css/App.css';
 
 function App() {
